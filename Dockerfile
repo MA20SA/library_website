48ff2a31c0414cc6cfbbc3034a0371bcff10a8a1
Masa Mansour <sahoh225@gmail.com>
2025-05-02 06:29:04 +0300

    LastChanges

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM php:8.1-apache
-
-# Install mysqli extension
-RUN docker-php-ext-install mysqli
-
-# Copy all your files into the web server directory
-COPY . /var/www/html/
-
-EXPOSE 80
--- includes/db.php
+++ includes/db.php
@@ -1,13 +1,7 @@
 <?php
 
-$host = 'sql12.freesqldatabase.com'; // Replace with your actual host
-$username = 'sql12776427';             // Replace with your database username
-$password = 'Please wait';         // Replace with your password
-$database = 'sql12776427';         // Replace with your database name
+$conn = mysqli_connect('localhost','root','','library');
 
-// $conn = mysqli_connect('localhost','root','','library');
-
-$conn = mysqli_connect($host, $username, $password, $database);
 
 // when $conn = false(there's error) this block happend --> !F = T
 if(!$conn){
