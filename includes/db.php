be2d4b7f03d435a8803ff64a50a15c946a6697fa
Masa Mansour <sahoh225@gmail.com>
2025-05-02 04:31:38 +0300

    Add dp

--- includes/db.php
+++ includes/db.php
@@ -1,5 +1,13 @@
 <?php
-$conn = mysqli_connect('localhost','root','','library');
+
+$host = 'sql12.freesqldatabase.com'; // Replace with your actual host
+$username = 'sql12776427';             // Replace with your database username
+$password = 'Please wait';         // Replace with your password
+$database = 'sql12776427';         // Replace with your database name
+
+// $conn = mysqli_connect('localhost','root','','library');
+
+$conn = mysqli_connect($host, $username, $password, $database);
 
 // when $conn = false(there's error) this block happend --> !F = T
 if(!$conn){
